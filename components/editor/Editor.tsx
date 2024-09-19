2f2fcea7d03837f7ee250e042c7328e07ee3fcc3
MoitreyoGhosh <moitreyoghosh@gmail.com>
2024-09-20 01:21:59 +0530

    Sticky Comments

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -22,7 +22,7 @@ import Loader from "../Loader";
 
 import FloatingToolbarPlugin from "./plugins/FloatingToolbarPlugin";
 import { useThreads } from "@liveblocks/react/suspense";
-// import Comments from '../Comments';
+import Comments from "../Comments";
 // import { DeleteModal } from '../DeleteModal';
 
 // Catch any errors that occur during Lexical updates and log them
@@ -83,7 +83,7 @@ export function Editor({
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
-            {/* <Comments /> */}
+            <Comments />
           </LiveblocksPlugin>
         </div>
       </div>
